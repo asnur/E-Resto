1966cbeac67880b16e924b574375889cc9a5e572
Asnur Ramdani <asnurramdhani12@gmail.com>
2022-06-26 17:34:24 +0700

    Upadate SQL

--- E-Resto.sql
+++ E-Resto.sql
@@ -47,9 +47,12 @@ CREATE TABLE `tbl_order` (
 CREATE TABLE `tbl_order_detail` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_order` bigint(20) DEFAULT NULL,
+  `id_menu` bigint(20) DEFAULT NULL,
   `quantity` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `fk_tbl_order_detail_menu` (`id_menu`),
   KEY `fk_tbl_order_order_details` (`id_order`),
+  CONSTRAINT `fk_tbl_order_detail_menu` FOREIGN KEY (`id_menu`) REFERENCES `tbl_menu` (`id`),
   CONSTRAINT `fk_tbl_order_order_details` FOREIGN KEY (`id_order`) REFERENCES `tbl_order` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -57,7 +60,7 @@ CREATE TABLE `tbl_payement` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` longtext,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 INSERT INTO `tbl_category` (`id`, `name`, `icon`) VALUES
 (1, 'Makanan', 'bibimbap.png');
@@ -77,21 +80,26 @@ INSERT INTO `tbl_meja` (`id`, `no_meja`) VALUES
 (6, 6);
 
 INSERT INTO `tbl_menu` (`id`, `name`, `price`, `id_category`, `foto`) VALUES
-(1, 'Es Teh', 2000, 2, NULL);
+(1, 'Es Teh', 2000, 2, 'es_teh.jpg');
 INSERT INTO `tbl_menu` (`id`, `name`, `price`, `id_category`, `foto`) VALUES
-(2, 'Mie', 10000, 1, NULL);
+(2, 'Mie', 10000, 1, 'mie.jpg');
 INSERT INTO `tbl_menu` (`id`, `name`, `price`, `id_category`, `foto`) VALUES
-(3, 'Bakso', 15000, 1, NULL);
+(3, 'Bakso', 15000, 1, 'bakso.jpg');
 INSERT INTO `tbl_menu` (`id`, `name`, `price`, `id_category`, `foto`) VALUES
-(4, 'Es Coklat', 3000, 2, NULL),
-(5, 'Kopi', 2000, 2, NULL),
-(6, 'Rujak Kangkung', 20000, 1, NULL);
+(4, 'Es Coklat', 3000, 2, 'es_coklat.jpeg'),
+(5, 'Kopi', 2000, 2, 'kopi.jpg'),
+(6, 'Rujak Kangkung', 20000, 1, 'rujak_kangkung.jpg');
 
 
 
 
 
-
+INSERT INTO `tbl_payement` (`id`, `name`) VALUES
+(1, 'Gopay');
+INSERT INTO `tbl_payement` (`id`, `name`) VALUES
+(2, 'BCA');
+INSERT INTO `tbl_payement` (`id`, `name`) VALUES
+(3, 'BNI');
 
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
